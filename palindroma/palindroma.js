2d284e649cbb248be9996cd622f2ed765d52938c
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-07 15:53:29 +0200

    [upd] function to print the word backwards

--- palindroma/palindroma.js
+++ palindroma/palindroma.js
@@ -3,7 +3,20 @@ console.log('sono connesso')
 
 const word = prompt("inserisci una parola e premi F12 per scoprire se è palindroma");
 
+console.log(`input preso dall'utente: ${word}` )
+
 function palindroma(word){
     
-    
+    //conterrà la parola invertita
+    let reverseWord = "";
+    //iteriamo all'inverso sulla parola inserita dall'utente
+    for (let i = word.length - 1; i >= 0; i--){
+        // si stampa word in posizone index al contrario su reverseWord
+        reverseWord += word[i];
+    }
+        console.log(`parola correttamente invertita: ${reverseWord}`);
+        return reverseWord;
+
 }
+
+palindroma(word);
